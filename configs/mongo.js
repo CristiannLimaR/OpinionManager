ef1ca60dddcafb02468d88a962708caff161cd37
CristiannLimaR <cristiannlima2@gmail.com>
2025-02-18 16:45:06 -0600

    default user

--- configs/mongo.js
+++ configs/mongo.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import User from "../src/user/user.model.js"
+import { hash } from "argon2";
 
 export const dbConection = async () => {
   try {
@@ -27,8 +29,35 @@ export const dbConection = async () => {
     await mongoose.connect(process.env.URI_MONGO, {
       serverSelectionTimeoutMS: 5000,
       maxPoolSize: 50,
-    });
+    }).then(createDeafultUser())
   } catch (error) {
     console.log("Database connection failded", error);
   }
 };
+
+const createDeafultUser = async() => {
+  try {
+  const user = await User.findOne({ username: 'admin' });
+
+    if (!user) {
+      
+      const cryptedPassword = await hash('admin1234');
+
+     
+      const defaultUser = new User({
+        name: 'admin',
+        username: 'admin',
+        surname: 'admin',
+        email: 'admin@gmail.com',
+        password: cryptedPassword
+      });
+
+      await defaultUser.save();
+      console.log('Default user created');
+    } else {
+      console.log('Default user already exists');
+    }
+  } catch (err) {
+    console.error('Error:', err);
+  }
+}
